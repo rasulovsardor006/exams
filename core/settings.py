fa39f57506a6125636f19e55f2a6680c83bd5e47
sardorasulov1525 <sardorrasulov1525@gmail..com>
2024-11-03 13:29:37 +0500

    Remove sensitive information

--- core/settings.py
+++ core/settings.py
@@ -168,11 +168,16 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sardorrasulov838@gmail.com'
-EMAIL_HOST_PASSWORD = 'lmmb lkrt xzhs tcsf'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 
